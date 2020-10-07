90726ee8cbe5ad95c4020d487881974e9f382bc8
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-07 17:13:35 +0100

    finished enumeration_traveller hw

--- homework/hw_enumeration_traveller_start/models/traveller.js
+++ homework/hw_enumeration_traveller_start/models/traveller.js
@@ -3,27 +3,47 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const firstLocation = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return firstLocation;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const lastLocation = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return lastLocation;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const myTravel = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return myTravel;
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const myTravel = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  });
+  return myTravel;
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const myTravel = this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  }, 0);
+  return myTravel;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const myTravel = this.journeys.map((journey) => {
+    return journey.transport;
+  });
+  const newTravel = new Set(myTravel); // gets rid of any duplicate values
+  console.log(newTravel);
+  return Array.from(newTravel); // takes Set & turns it back into array
 };
 
 
